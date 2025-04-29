538f01a04af5f63dd28e8e17b70ab2e53027c78c
faiza sharif <161868234+faizasharif7@users.noreply.github.com>
2025-04-29 07:35:57 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -271,7 +271,7 @@ def create_visulations(stats):
 
 #load library
 load_library()
-st.sidebar.markdown("<h1 style='text-align: center;'> Navigation</h1>", unsafe_allow_html=True)
+st.sidebar.markdown("<h1 style='text-align: center'> Navigation</h1>", unsafe_allow_html=True)
 lottie_book = load_lottieurl("https://assests9.lotttieflies.com/temp/1f20_aKAfIn.json")
 if lottie_book:
     with st.sidebar:

9b49b0e7c2a38673f09219e62ebc59f565d1999f
faiza sharif <161868234+faizasharif7@users.noreply.github.com>
2025-04-30 08:17:22 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -307,7 +307,6 @@ if st.session_state.current_view == "add":
         
         with col2:
             genre = st.selectbox("Genre", [
-                ""
                 "Fiction", "Non-Fiction", "Science", "Technology", "Fantasy","Romance", "Poetry", "Self-help", "Art", "Religion", "History", "Other"
             ])
             read_status = st.radio("Read Status", ["Read", "Unread"], horizontal=True)
